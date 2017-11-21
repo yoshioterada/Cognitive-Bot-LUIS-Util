c8def06f5ef295801c41cb839dc78f34848d5332
Yoshio Terada <tyoshio2002@yahoo.co.jp>
2017-11-22 01:59:04 +0900

    Added the License statement.
    Added the License statement.

--- src/main/java/com/yoshio3/services/FaceDetectorService.java
+++ src/main/java/com/yoshio3/services/FaceDetectorService.java
@@ -1,7 +1,17 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2017 Yoshio Terada
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.yoshio3.services;
 
@@ -64,13 +74,9 @@ public class FaceDetectorService implements Serializable {
                 .async()
                 .post(Entity.entity(entity, MediaType.APPLICATION_JSON_TYPE));
         Response response = futureResponse.get();
-//        return response.readEntity(String.class);
-        //[
         //{"faceId":"de0cd25d-7834-46cc-9fce-9c757f71eb1c",
         //"faceRectangle":{"top":147,"left":326,"width":133,"height":133},
         //"faceAttributes":{"gender":"male","age":33.4}}]
-
-        
         return jobForFace(response);
     }
 
