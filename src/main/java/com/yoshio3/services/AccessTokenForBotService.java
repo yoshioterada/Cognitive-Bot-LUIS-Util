5e17330818a5a40308693e6329d87ba7d0551e39
Yoshio Terada <tyoshio2002@yahoo.co.jp>
2017-11-24 22:05:05 +0900

    Added the LICENS as MIT License.
    Added the LICENS as MIT License.

--- src/main/java/com/yoshio3/services/AccessTokenForBotService.java
+++ src/main/java/com/yoshio3/services/AccessTokenForBotService.java
@@ -1,26 +1,37 @@
-/*
- * Copyright 2017 Yoshio Terada
+/**
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Copyright (c) 2017 Yoshio Terada
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
-package com.yoshio3.services.util;
+package com.yoshio3.services;
 
 import com.yoshio3.rest.entities.bot.AccessTokenEntity;
-import com.yoshio3.rest.entities.bot.BotObjectMapperProvider;
+import com.yoshio3.services.util.PropertyReaderService;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -28,13 +39,12 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedHashMap;
 import javax.ws.rs.core.Response;
-import org.glassfish.jersey.jackson.JacksonFeature;
 
 /**
  *
  * @author Yoshio Terada
  */
-public class AccessTokenForBotService extends BasicRESTService {
+public class AccessTokenForBotService {
 
     private final static Logger LOGGER = Logger.getLogger(AccessTokenForBotService.class.getName());
     
@@ -53,7 +63,7 @@ public class AccessTokenForBotService extends BasicRESTService {
     private static String accessToken;
     private static LocalDateTime oldDate;
 
-    public static String getAccesToken() {
+    public String getAccesToken() {
         if (accessToken == null) {
             createToken();
         } else {
@@ -69,16 +79,15 @@ public class AccessTokenForBotService extends BasicRESTService {
         return accessToken;
     }
 
-    private static void createToken() {
+    private String createToken() {
         accessToken = getAccesTokenFromServer().getAccess_token();
         oldDate = LocalDateTime.now();        
         LOGGER.log(Level.FINE, "CREATED or REFRESHED Token : {0}\tDate: {1}", new Object[]{accessToken, oldDate});
+        return accessToken;
     }
 
     private static AccessTokenEntity getAccesTokenFromServer() {
         Client client = ClientBuilder.newBuilder()
-                .register(BotObjectMapperProvider.class)
-                .register(JacksonFeature.class)
                 .build();
 
         MultivaluedHashMap<String, String> formdata = new MultivaluedHashMap<>();
@@ -97,44 +106,12 @@ public class AccessTokenForBotService extends BasicRESTService {
 
         AccessTokenEntity readEntity;
         if (family == Response.Status.Family.SUCCESSFUL) {
-            readEntity = response.readEntity(AccessTokenEntity.class);
-            return readEntity;
+            String jsonData = response.readEntity(String.class); 
+            Jsonb jsonb = JsonbBuilder.create();            
+            AccessTokenEntity tokenEntity = jsonb.fromJson(jsonData, AccessTokenEntity.class);
+            return tokenEntity;
         } else {
             return null;
         }
     }
-
-
-    
-    /*
-POST https://login.microsoftonline.com/botframework.com/oauth2/v2.0/token
-Host: login.microsoftonline.com
-Content-Type: application/x-www-form-urlencoded
-
-grant_type=client_credentials&client_id=MICROSOFT-APP-ID&client_secret=MICROSOFT-APP-PASSWORD&scope=https%3A%2F%2Fapi.botframework.com%2F.default    
-    */
-
-    public void foo(){
-        Client client = ClientBuilder.newBuilder()
-                .register(BotObjectMapperProvider.class)
-                .register(JacksonFeature.class)
-                .build();
-
-        MultivaluedHashMap<String, String> formdata = new MultivaluedHashMap<>();
-        formdata.putSingle("grant_type", "client_credentials");
-        formdata.putSingle("client_id", "e3381dec-15fb-40f0-9d18-23b0a2213b7f");
-        formdata.putSingle("client_secret", "tjrtQRH1**|xfqLGBD5753]");
-        formdata.putSingle("scope", SCOPE);
-
-        WebTarget target = client
-                .target("https://login.microsoftonline.com/botframework.com/oauth2/v2.0/token");
-        Response response = target
-                .request(MediaType.APPLICATION_FORM_URLENCODED)
-                .post(Entity.form(formdata));
-
-        String readEntity = response.readEntity(String.class);
-        System.out.println(readEntity);
-    }
-    
-
 }
--- src/main/java/com/yoshio3/services/BotService.java
+++ src/main/java/com/yoshio3/services/BotService.java
@@ -1,48 +1,52 @@
-/*
- * Copyright 2017 Yoshio Terada
+/**
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Copyright (c) 2017 Yoshio Terada
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 package com.yoshio3.services;
 
-import com.yoshio3.services.util.BasicRESTService;
 import com.yoshio3.rest.entities.bot.childelements.Conversation;
 import com.yoshio3.rest.entities.bot.childelements.From;
 import com.yoshio3.rest.entities.bot.childelements.Members;
 import com.yoshio3.rest.entities.bot.MessageBackToBotFramework;
 import com.yoshio3.rest.entities.bot.MessageFromBotFrameWork;
-import com.yoshio3.rest.entities.bot.BotObjectMapperProvider;
 import com.yoshio3.rest.entities.bot.childelements.Recipient;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.time.Instant;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.enterprise.context.RequestScoped;
-import javax.ws.rs.client.Client;
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import org.glassfish.jersey.jackson.JacksonFeature;
 
 /**
  *
  * @author Yoshio Terada
  */
-@RequestScoped
-public class BotService extends BasicRESTService {
+public class BotService{
 
     private final static Logger LOGGER = Logger.getLogger(BotService.class.getName());
     private final static String COVERSATIONS = "/v3/conversations/";
@@ -51,7 +55,9 @@ public class BotService extends BasicRESTService {
     public void sendResponse(MessageFromBotFrameWork requestMessage, String accessToken, String message) {
         MessageBackToBotFramework creaeteResponseMessage = creaeteResponseMessage(requestMessage, message);
         LOGGER.log(Level.FINE, "Back Messages:{0}", creaeteResponseMessage.toString());
-
+        Jsonb jsonb = JsonbBuilder.create(); 
+        String jsonData = jsonb.toJson(creaeteResponseMessage);
+        
         String serviceUrl = requestMessage.getServiceUrl();
 
         String id = requestMessage.getId();
@@ -61,19 +67,14 @@ public class BotService extends BasicRESTService {
         } catch (UnsupportedEncodingException ex) {
             LOGGER.log(Level.SEVERE, "UTF-8 is not supported", ex);
         }
-
         String uri = serviceUrl + COVERSATIONS + convID + ACTIVITIES; // + id;
 
-        Client client = ClientBuilder.newBuilder()
-                .register(BotObjectMapperProvider.class)
-                .register(JacksonFeature.class)
-                .build();
-        WebTarget target = client
-                .target(uri);
-        Response response = target
+        Response response = ClientBuilder.newBuilder()
+                .build()
+                .target(uri)
                 .request(MediaType.APPLICATION_JSON)
                 .header("Authorization", "Bearer " + accessToken)
-                .post(Entity.entity(creaeteResponseMessage, MediaType.APPLICATION_JSON));
+                .post(Entity.entity(jsonData, MediaType.APPLICATION_JSON));
 
         if (!isRequestSuccess(response)) {
             LOGGER.log(Level.SEVERE, "{0}:{1}", new Object[]{response.getStatus(), response.readEntity(String.class)});
@@ -81,7 +82,7 @@ public class BotService extends BasicRESTService {
         }
     }
 
-    private MessageBackToBotFramework creaeteResponseMessage(MessageFromBotFrameWork requestMessage, String message) {
+    public MessageBackToBotFramework creaeteResponseMessage(MessageFromBotFrameWork requestMessage, String message) {
         MessageBackToBotFramework resMsg = new MessageBackToBotFramework();
         resMsg.setType("message");
 
@@ -114,5 +115,21 @@ public class BotService extends BasicRESTService {
         resMsg.setText(message);
         resMsg.setReplyToId(requestMessage.getId());
         return resMsg;
+    }
+
+    private boolean isRequestSuccess(Response response) {
+        Response.StatusType statusInfo = response.getStatusInfo();
+        Response.Status.Family family = statusInfo.getFamily();
+        return family != null && family == Response.Status.Family.SUCCESSFUL;
+    }
+
+    /*
+     Operate when the REST invocaiton failed.
+     */
+    private void handleIllegalState(Response response)
+            throws IllegalStateException {
+        String error = response.readEntity(String.class);
+        Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, error);
+        throw new IllegalStateException(error);
     }
 }
